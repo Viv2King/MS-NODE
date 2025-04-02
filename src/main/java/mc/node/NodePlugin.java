6b6f0c748226f687d4acf8fe7fcdfd9f498c1dc5
David <dawoodgame85@gmail.com>
2025-04-02 13:21:04 +0300

    NODE CREATOR

--- src/main/java/mc/node/NodePlugin.java
+++ src/main/java/mc/node/NodePlugin.java
@@ -1,5 +1,6 @@
 package mc.node;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NodePlugin extends JavaPlugin {
@@ -7,11 +8,13 @@ public final class NodePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Bukkit.getConsoleSender().sendMessage("[NODE] Plugin Enabled");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getConsoleSender().sendMessage("[NODE] Plugin Disabled");
     }
 }
